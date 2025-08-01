af42b5d74183a0cedb5dd2f4b18ccea623a800b6
Yui0126 <yui.naruse.94@gmail.com>
2025-08-01 22:15:36 +0900

    syntax changing

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -36,8 +36,8 @@ class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPE = [
-        ('SEDAN','Sedan'),
-        ('SUV','SUV'),
+        ('SEDAN','Sedan'), 
+        ('SUV','SUV'), 
         ('WAGON','WAGON')
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPE, default='SUV')
@@ -49,3 +49,4 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name
+    
