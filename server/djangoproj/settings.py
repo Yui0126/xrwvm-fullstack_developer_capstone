acfaf06f46cd9db84385f92c2fd9e5cf259f9461
Yui0126 <yui.naruse.94@gmail.com>
2025-08-01 11:16:51 -0400

    docker

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,9 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost',
-                 'https://']
-CSRF_TRUSTED_ORIGINS = ['https://']
+ALLOWED_HOSTS = ['localhost','https://yuinaruse94-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://yuinaruse94-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -95,7 +94,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django',
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
